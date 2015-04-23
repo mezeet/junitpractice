bcec0706ef4385bdd53a298cc4fd47b280cf0a62
mezeet <admin@mezeet.com>
2015-04-23 10:39:43 +0900

    기본 계산 테스트 케이스를 기반으로 실제 클래스 생성

---
+++ src/main/java/test/BasicMath.java
@@ -0,0 +1,15 @@
+package test;
+
+public class BasicMath {
+
+	public Object addition(int i, int j) {
+		
+		return null;
+	}
+
+	public Object multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
