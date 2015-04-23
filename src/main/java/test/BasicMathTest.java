899bb8422e7e976e7f476078debbe1dce1654730
mezeet <admin@mezeet.com>
2015-04-23 14:45:45 +0900

    1. javadoc 설명 바꿈 	- setUp()    : 시작 시 공통작업 	- tearDown() : 종료 시 공통작업
    2. 테스트 케이스 함수 작동하도록 변경
    	assertEquals(함수실행결과,맞는 결과값)

--- src/main/java/test/BasicMathTest.java
+++ src/main/java/test/BasicMathTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-
 import junit.framework.TestCase;
 
 /**
@@ -10,20 +9,26 @@ import junit.framework.TestCase;
  */
 import org.junit.Test;
 
+/**
+ *  기본 연산 테스트 케이스
+ * @author mezeet
+ * @see http://bcho.tistory.com/86
+ */
+
 public class BasicMathTest extends TestCase {
 
 	
 	private BasicMath testItem ;
 	
 	/**
-	 *  테스트 할 클래스를 연결
+	 *  테스트 전 실행할 공통 작업 - 예 ) 커넥션 생성 등
 	 */
 	protected void setUp() throws Exception {
 		testItem = new BasicMath();
 	}
 	
 	/**
-	 *  테스트 할 클래스와 테스트 케이스 분리하는 메소드
+	 *  테스트 후 실행할 공통 작업 - 예 ) DB 커넥션 닫기
 	 */
 	protected void tearDown() throws Exception {
 		testItem = null ;
