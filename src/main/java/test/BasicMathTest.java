a5d3c39919bacea402142e8863aec3cb8b0ac631
mezeet <admin@mezeet.com>
2015-04-23 10:38:41 +0900

    기본 계산 테스트 케이스 작성

---
+++ src/main/java/test/BasicMathTest.java
@@ -0,0 +1,22 @@
+package test;
+
+public class BasicMathTest {
+
+	private BasicMath testItem ;
+	protected void setUp() throws Exception {
+		testItem = new BasicMath();
+	}
+	protected void tearDown() throws Exception {
+		testItem = null ;
+	}
+	@Test
+	public void testAddition() {
+		assertEquals(5, testItem.addition(2, 3));
+	}
+	@Test
+	public void testMultiply() {
+		assertEquals(6, testItem.multiply(2, 3));
+	}
+		
+	
+}
